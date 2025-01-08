13d8bc834e693029f6e19ddfb82b48619fd18bef
Paulo <paulo@blockchain.com>
2025-01-08 16:28:38 +0100

    Update package for release

--- Package.swift
+++ Package.swift
@@ -12,13 +12,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WalletCore",
-            url: "https://github.com/dchatzieleftheriou-bc/wallet-core/releases/download/4.1.17-blockchain/WalletCore.xcframework.zip",
-            checksum: "00"
+            url: "https://github.com/dchatzieleftheriou-bc/wallet-core/releases/download/4.2.0-blockchain/WalletCore.xcframework.zip",
+            checksum: "506eb452cc5fdd6d86d5db99985d4ccc15fb3500f0814aff249bce7b5de44e02"
         ),
         .binaryTarget(
             name: "SwiftProtobuf",
-            url: "https://github.com/dchatzieleftheriou-bc/wallet-core/releases/download/4.1.17-blockchain/SwiftProtobuf.xcframework.zip",
-            checksum: "00"
+            url: "https://github.com/dchatzieleftheriou-bc/wallet-core/releases/download/4.2.0-blockchain/SwiftProtobuf.xcframework.zip",
+            checksum: "03295a99b844bde3ef7ff8d73184860a236da43b83a7cd352d6a4ed9646aeee5"
         )
     ]
 )
